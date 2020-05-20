f73bc194fe7c6b059a156a42fd6014c84b9f44a2
sjtg <chipilej@gmail.com>
2020-05-20 19:34:59 +0200

    Created simple django project for covid_19 map

---
+++ covid_19_chart_map/covid_19_chart_map_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Covid19ChartMapAppConfig(AppConfig):
+    name = 'covid_19_chart_map_app'
