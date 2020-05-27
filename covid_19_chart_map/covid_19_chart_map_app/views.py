c6502c1d7e072fd512b9d6672d1b4fbf48e2fc90
sjtg <chipilej@gmail.com>
2020-05-27 14:53:50 +0200

    Added the following: 1: Added url for the home page 2: Created template page 3: Created views for the accessing the api

--- covid_19_chart_map/covid_19_chart_map_app/views.py
+++ covid_19_chart_map/covid_19_chart_map_app/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def covid(request):
+    user = {}
+    if 'countryname' in request.GET:
+        countryname = request.GET['countryname']
+        url = 'https://covid19.mathdro.id/api/countries/%s' % countryname
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'core/index.html', {'user': user})
